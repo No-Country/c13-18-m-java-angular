9769a852599acf72ffb243d8546d64632fd10b60
BrUnix <br1.sturniolo@gmail.com>
2023-08-18 12:42:10 -0300

    [FIX]regenesis y cambio el .gitignore por uno basico de spring

---
+++ backend/src/main/java/com/grupo18/nocountry/greenpoint/GreenpointApplication.java
@@ -0,0 +1,13 @@
+package com.grupo18.nocountry.greenpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenpointApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/grupo18/nocountry/greenpoint/GreenpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo18.nocountry.greenpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
