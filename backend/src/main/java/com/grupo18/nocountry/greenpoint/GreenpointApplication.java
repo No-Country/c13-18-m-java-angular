92ae5edbe250ccaf9ddaea13ba9303b3db269118
StonyKing <ramiro_hero@hotmail.com>
2023-08-21 16:12:36 -0300

    Agregado manejo de errores global

--- backend/src/main/java/com/grupo18/nocountry/greenpoint/GreenpointApplication.java
+++ backend/src/main/java/com/grupo18/nocountry/greenpoint/GreenpointApplication.java
@@ -1,5 +1,6 @@
 package com.grupo18.nocountry.greenpoint;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreenpointApplication {
 
 	public static void main(String[] args) {
+
+		PropertyConfigurator.configure("log4j.properties");
 		SpringApplication.run(GreenpointApplication.class, args);
 	}
 
