f3de4813af52e3aea68f38851fe706c416b172f2
Brunix <br1.sturniolo@gmail.com>
2023-08-24 12:34:48 -0300

    [ADD]dto, y CRUD

--- backend/src/main/java/com/grupo18/nocountry/greenpoint/recyclable/RecyclableController.java
+++ backend/src/main/java/com/grupo18/nocountry/greenpoint/recyclable/RecyclableController.java
@@ -1,12 +1,14 @@
 package com.grupo18.nocountry.greenpoint.recyclable;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/recyclable")
@@ -16,7 +18,36 @@ public class RecyclableController {
     private final RecyclableService recyclableService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Recyclable> getRecyclableById(@PathVariable Long id) {
-        return ResponseEntity.ok(recyclableService.getById(id));
+    public ResponseEntity<?> getRecyclableById(@PathVariable Long id) {
+        Optional<Recyclable> recyclableOptional = recyclableService.getById(id);
+
+        if (recyclableOptional.isPresent()){
+            Recyclable recyclable = recyclableOptional.get();
+
+            // Todo usar libreria de mapeo dto, en vez de hacerlo manual
+            RecyclableDTO recyclableDTO = RecyclableDTO.builder()
+                    .id(recyclable.getId())
+                    .recyclableType(recyclable.getRecyclableType())
+                    .points(recyclable.getPoints())
+                    .build();
+
+            return ResponseEntity.ok(recyclableDTO);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll() {
+
+        List<RecyclableDTO> recyclableDTOS = recyclableService.findAll()
+                .stream()
+                .map(recyclable -> RecyclableDTO.builder()
+                        .id(recyclable.getId())
+                        .recyclableType(recyclable.getRecyclableType())
+                        .points(recyclable.getPoints())
+                        .build()
+                ).toList();
+
+        return ResponseEntity.ok(recyclableDTOS);
     }
 }
--- backend/src/main/java/com/grupo18/nocountry/greenpoint/recyclable/RecyclableService.java
+++ backend/src/main/java/com/grupo18/nocountry/greenpoint/recyclable/RecyclableService.java
@@ -4,13 +4,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class RecyclableService {
 
     private final RecyclableRepository recyclableRepository;
 
-    public Recyclable getById(Long id) {
-        return recyclableRepository.findById(id).get();
+    public Optional<Recyclable> getById(Long id) {
+        return recyclableRepository.findById(id);
+    }
+
+    public List<Recyclable> findAll() {
+        return recyclableRepository.findAll();
+    }
+
+    public void save (Recyclable recyclable) {
+        recyclableRepository.save(recyclable);
+    }
+
+    public void deleteById (Long id) {
+        recyclableRepository.deleteById(id);
     }
 }
