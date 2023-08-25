18f5d857934005d53d10009cd4975c8caa8f6fd6
federico42o <fedes7777@gmail.com>
2023-08-25 19:38:38 -0300

    delete sample code

--- backend/src/main/java/com/grupo18/nocountry/greenpoint/repository/TemporalSampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo18.nocountry.greenpoint.repository;
-
-import com.grupo18.nocountry.greenpoint.model.TemporalSample;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TemporalSampleRepository extends JpaRepository<TemporalSample, Long> {
-}
