3c3caf4757234aacedc37bbf7f2efae1c7e114b3
Brunix <br1.sturniolo@gmail.com>
2023-08-25 11:29:18 -0300

    [FIX]pathController,y valor del reciclable por body

--- backend/src/main/java/com/grupo18/nocountry/greenpoint/recyclable/Recyclable.java
+++ backend/src/main/java/com/grupo18/nocountry/greenpoint/recyclable/Recyclable.java
@@ -21,25 +21,4 @@ public class Recyclable {
     private RecyclableType recyclableType;
     private int points;
 
-    public Recyclable(RecyclableType recyclableType) {
-        this.recyclableType = recyclableType;
-        this.points = calculatePoints(recyclableType);
-    }
-
-    private int calculatePoints(RecyclableType recyclableType) {
-        switch (recyclableType) {
-            case METAL:
-                return 5;
-            case MADERA:
-                return 3;
-            case ORGANICO:
-                return 2;
-            case VIDRIO:
-                return 1;
-            case PLASTICO:
-                return 4;
-            default:
-                return 0; // tipo no reconocido
-        }
-    }
 }
