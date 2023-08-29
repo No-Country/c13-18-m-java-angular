ded4f8b44cb38e902570b6a65f3725fb6408335e
Brunix <br1.sturniolo@gmail.com>
2023-08-29 12:21:42 -0300

    [FIX]genesis catalogue

--- backend/src/main/java/com/grupo18/nocountry/greenpoint/catalogue/Catalogue.java
+++ backend/src/main/java/com/grupo18/nocountry/greenpoint/catalogue/Catalogue.java
@@ -1,4 +1,13 @@
 package com.grupo18.nocountry.greenpoint.catalogue;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Catalogue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
