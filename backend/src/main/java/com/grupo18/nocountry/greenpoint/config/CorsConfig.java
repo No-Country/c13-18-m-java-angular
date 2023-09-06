f89143312ea2c257c633bb57727904760897349d
Brunix <br1.sturniolo@gmail.com>
2023-09-06 15:01:47 -0300

    Revert "[ADD]cors swagger"
    This reverts commit f2fcca2fdc4372d72df3b0739f4a9f7c3db3b0db.

--- backend/src/main/java/com/grupo18/nocountry/greenpoint/config/CorsConfig.java
+++ backend/src/main/java/com/grupo18/nocountry/greenpoint/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","http://190.124.144.225","http://localhost","http://greenpoint.zanity.net","http://190.124.144.225:8080/swagger-ui/index.html")
+                        .allowedOrigins("http://localhost:4200","http://190.124.144.225","http://localhost","http://greenpoint.zanity.net")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
